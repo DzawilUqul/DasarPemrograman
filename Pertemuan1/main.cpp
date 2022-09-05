bacfc39f72ccea95a8bc84c48a66549671fb667b
DzawilUqul <112853502+DzawilUqul@users.noreply.github.com>
2022-09-05 21:51:12 +0700

    Add files via upload

---
+++ Pertemuan1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama : Dzawil Uqul" << endl;
+    cout << "NIM : A11.2022.14141" << endl;
+    cout << "Alamat : Sinanggul Rt44 Rw08, Mlonggo, Jepara" << endl;
+    cout << "Asal Sekolah : SMA N 1 Jepara" << endl;
+    return 0;
+}
