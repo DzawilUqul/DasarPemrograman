c35b807eb7e04287ad8b71a096d96474c9dcbbba
DzawilUqul <112853502+DzawilUqul@users.noreply.github.com>
2022-09-12 15:07:12 +0700

    Add files via upload

---
+++ Pertemuan3/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    int x;
+    x=21;
+    int y =2;
+    string k = "XL";
+    float z = 185.1;
+
+    cout<<"\nUmur Saya = "<< x ;
+    cout<<"\nTinggi Saya = "<< z ;
+    cout<<"\nUkuran Baju = "<< k ;
+    return 0;
+}
