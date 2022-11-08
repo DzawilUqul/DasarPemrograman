57357b706e2a4da7229fed27403930a2b3621604
DzawilUqul <112853502+DzawilUqul@users.noreply.github.com>
2022-11-08 12:23:28 +0700

    Update main.cpp

--- Pertemuan15/main.cpp
+++ Pertemuan15/main.cpp
@@ -12,8 +12,8 @@ int main(){
         permen = 5;
     }else if (x==1000){             // Harga permen 1000
         permen = uang/1000;
-    }else if (x==300){
-        while(x<=uang){             // Harga permen 300
+    }else if (x==300){              // Harga permen 300
+        while(x<=uang){             
             if(x>uang-300){
             cout<<x<<". ";
             x = x+300;
